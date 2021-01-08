77a5b4c4e4eb44287cab7ac818ebc95a6f8df932
Abdelaziz <abdelaziz.elrharbi@outlook.fr>
2021-01-08 16:57:47 +0100

    Erreur de compréhension de l'énoncé

--- ex03/mycp.ts
+++ ex03/mycp.ts
@@ -48,6 +48,7 @@ function copyDir(src:string, dest:string){
 
 function mycp() {
 
+    //Le cas d'un cp de fichier
     if(process.argv.length == 4){
 
         const source = path.join(process.cwd(),process.argv[2])
@@ -60,15 +61,74 @@ function mycp() {
             }
             else{
 
-                copyDir(source, dest)
+                if(fs.lstatSync(source).isDirectory()){
+
+                    console.log('mycp.ts: -r non spécifié')
+                }
+                else{
+                    console.log('Votre fichier n\'existe pas')
+                }
             }
+                
         }
         else{
-            console.log(source+' n\'existe pas !')
+            if(process.argv[2] == '-r'){
+
+                console.log('Il manque un paramètre')
+            }
+            else{
+
+                console.log(source+' n\'existe pas !')
+            }
         }
     }
     else{
-        console.log('Il manque ou il y a trop de paramètres')
+        
+        //Le cas d'un cp de directory
+        if(process.argv.length == 5){
+            
+            const source = path.join(process.cwd(),process.argv[3])
+            if(fs.existsSync(source)){
+                
+                const dest = path.join(process.cwd(),process.argv[4])
+                if(fs.lstatSync(source).isDirectory()){
+
+                    if(process.argv[2] == '-r'){
+
+                        copyDir(source, dest)
+                    }
+                    else{
+                        
+                        console.log('mycp.ts: -r non spécifié')
+                    }
+                }
+                else{
+
+                    if(fs.lstatSync(source).isFile()){
+
+                        console.log('L\'option \'-r\' est de trop')
+                    }
+                    else{
+                        console.log('Votre directory n\'existe pas')
+                    }
+                }
+                    
+            }
+            else{
+                if(process.argv[2] !== '-r'){
+
+                    console.log('mycp.ts: -r non spécifié')
+                }
+                else{
+
+                    console.log(source+' n\'existe pas !')
+                }
+            }
+        }
+        else{
+
+            console.log('Il manque ou il y a trop de paramètres')
+        }
     }
 }
 
